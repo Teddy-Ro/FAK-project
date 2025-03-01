ee4a03e1f025208edc99aaa7fb592657d1d22a2c
Ted <rozanovfedya@gmail.com>
2025-03-01 22:16:04 +0300

    we started FAK

---
+++ build/CMakeFiles/FAK.dir/FAK_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/FAK.dir/FAK_autogen/mocs_compilation.cpp.o: \
+ /home/ted/BMSTUproga/FAK-project/build/FAK_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CMakeFiles/FAK_autogen_timestamp_deps.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FAK_autogen_timestamp_deps"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/FAK_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
